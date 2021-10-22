a72cfcec9989bfd7b503a3c2fec169056430305a
NastasjaBakovic199 <nastasjabakovic199@gmail.com>
2021-10-22 20:05:34 +0200

    dadat logout

--- inc/header.php
+++ inc/header.php
@@ -19,6 +19,7 @@ include "function/init.php";
 	<li><a href="index.php">Home</a></li>
 	<li><a href="login.php">Login</a></li>
 	<li><a href="register.php">Register</a></li>
+	<li><a href="logout.php">Logout</a></li>
 </ul>
 	
 <hr>
